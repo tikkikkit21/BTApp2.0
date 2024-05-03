556df9fc56af0d5b1890ad0a0576d0b3cca5843b
tikkikkit21 <tikkikkit21@gmail.com>
2024-05-03 17:14:01 -0400

    Add map view

--- App.js
+++ App.js
@@ -77,8 +77,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
-        paddingTop: 50
+        backgroundColor: 'white'
     },
     tabBar: {
         backgroundColor: '#861F41',
--- Map.js
+++ Map.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import MapView, { Marker } from 'react-native-maps';
 
 export default function Map() {
     return (
-        <Text>This is the map</Text>
-    )
+        <MapView
+            style={styles.map}
+        />
+    );
 }
+
+const styles = StyleSheet.create({
+    map: {
+        width: '100%',
+        height: '100%',
+    },
+});
