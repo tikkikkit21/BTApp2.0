311613dd786c887988a061fa80efb7d227483614
tikkikkit21 <tikkikkit21@gmail.com>
2024-05-03 17:15:21 -0400

    Center map on Buruss hall

--- Map.js
+++ Map.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, StyleSheet } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
 
 export default function Map() {
+    const [mapRegion, setMapRegion] = useState({
+        latitude: 37.227468937500895,
+        longitude: -80.42357646125542,
+        latitudeDelta: 0.051202637986392574,
+        longitudeDelta: 0.03720943536600885,
+    });
+
     return (
         <MapView
             style={styles.map}
+            region={mapRegion}
+            onRegionChangeComplete={(region) => setMapRegion(region)}
+            showsUserLocation={true}
         />
     );
 }
