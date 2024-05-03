8a30c3181243bc0685acc8cc19e11ae5339f8c7b
tikkikkit21 <tikkikkit21@gmail.com>
2024-05-03 14:50:45 -0400

    Create basic bottom sheet

--- tabs/RoutesTab.js
+++ tabs/RoutesTab.js
@@ -1,10 +1,40 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useMemo, useRef, useCallback } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 
 export default function RoutesTab() {
+    const snapPoints = useMemo(() => ['27%', '50%', '70%', '95%'], []);
+    const bottomSheetRef = useRef(null);
+
+    const handleSheetChanges = useCallback((index) => {
+        console.log('handleSheetChanges', index);
+    }, []);
+
+
     return (
-        <View>
-            <Text>This is the routestab tab</Text>
+        <View style={styles.container}>
+            <BottomSheet
+                ref={bottomSheetRef}
+                onChange={handleSheetChanges}
+                snapPoints={snapPoints}
+            >
+                <BottomSheetView style={styles.contentContainer}>
+                    <Text>Awesome 🎉</Text>
+                </BottomSheetView>
+            </BottomSheet>
         </View>
     );
+
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: 'grey',
+    },
+    contentContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+});
