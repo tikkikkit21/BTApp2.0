ea800b411781f43ca16031d4278f79a35b08acce
Tikki <35748816+tikkikkit21@users.noreply.github.com>
2024-05-03 17:10:11 -0400

    Add tab bar (#2)
    Create a basic tab bar on bottom of app while preserving the same
    background across tabs

--- App.js
+++ App.js
@@ -1,11 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, useWindowDimensions } from 'react-native';
+import { TabView, TabBar, SceneMap } from 'react-native-tab-view';
+import { Entypo, FontAwesome5, FontAwesome6 } from '@expo/vector-icons';
+import RoutesTab from './tabs/RoutesTab';
+
+const renderScene = SceneMap({
+    home: () => null,
+    routes: () => <RoutesTab />,
+    plan: () => null,
+    settings: () => null
+});
 
 export default function App() {
+    const layout = useWindowDimensions();
+
+    const [index, setIndex] = React.useState(0);
+    const [routes] = useState([
+        { key: 'home', title: 'Home' },
+        { key: 'routes', title: 'Routes' },
+        { key: 'plan', title: 'Plan a Trip' },
+        { key: 'settings', title: 'Settings' },
+    ]);
+
+    function renderTabBar(props) {
+        return (
+            <TabBar
+                {...props}
+                indicatorStyle={{ opacity: 0 }}
+                style={styles.tabBar}
+                renderLabel={({ route, focused, color }) => {
+                    let icon;
+                    switch (route.key) {
+                        case "home":
+                            icon = <Entypo name="map" size={24} color={color} />;
+                            break;
+                        case "routes":
+                            icon = <FontAwesome6 name="route" size={24} color={color} />;
+                            break;
+                        case "plan":
+                            icon = <FontAwesome5 name="map-marked-alt" size={24} color={color} />;
+                            break;
+                        case "settings":
+                            icon = <FontAwesome6 name="gear" size={24} color={color} />
+                            break;
+                    }
+
+                    return (
+                        <View style={styles.label}>
+                            {icon}
+                            <Text style={{ color: color }}>{route.title}</Text>
+                        </View>
+                    );
+                }}
+            />
+        );
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
+            <Text>
+                This is the background text
+            </Text>
+            <TabView
+                navigationState={{ index, routes }}
+                renderScene={renderScene}
+                onIndexChange={setIndex}
+                initialLayout={{ width: layout.width }}
+                tabBarPosition='bottom'
+                swipeEnabled={false}
+                animationEnabled={false}
+                renderTabBar={renderTabBar}
+            />
         </View>
     );
 }
@@ -13,8 +78,16 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: 'white',
+        paddingTop: 50
     },
+    tabBar: {
+        backgroundColor: '#861F41',
+        paddingBottom: 10
+    },
+    label: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
 });
---
+++ tabs/RoutesTab.js
@@ -0,0 +1,38 @@
+import React, { useMemo, useRef, useCallback } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
+
+export default function RoutesTab() {
+    const snapPoints = useMemo(() => ['27%', '50%', '70%', '95%'], []);
+    const bottomSheetRef = useRef(null);
+
+    // const handleSheetChanges = useCallback((index) => {
+    //     console.log('handleSheetChanges', index);
+    // }, []);
+
+
+    return (
+        <BottomSheet
+            ref={bottomSheetRef}
+            // onChange={handleSheetChanges}
+            snapPoints={snapPoints}
+        >
+            <BottomSheetView style={styles.contentContainer}>
+                <Text>This is the routes list</Text>
+            </BottomSheetView>
+        </BottomSheet>
+    );
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: 'grey',
+    },
+    contentContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
+});
