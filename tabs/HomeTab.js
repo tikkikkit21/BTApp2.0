c56d92567481fc46dc3a458429f036976abf2f87
tikkikkit21 <tikkikkit21@gmail.com>
2024-05-03 14:25:29 -0400

    Create basic tab files

---
+++ tabs/HomeTab.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function HomeTab() {
+    return (
+        <View>
+            <Text>This is the home tab</Text>
+        </View>
+    );
+}
---
+++ tabs/RoutesTab.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function RoutesTab() {
+    return (
+        <View>
+            <Text>This is the routestab tab</Text>
+        </View>
+    );
+}
