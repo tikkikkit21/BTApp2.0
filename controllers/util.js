9ee56efd0fcca05fe9185661b06d42b5a298ff9d
tikkikkit21 <tikkikkit21@gmail.com>
2024-05-04 09:55:24 -0400

    Create util file

---
+++ controllers/util.js
@@ -0,0 +1,15 @@
+/**
+ * Formats BT app data by removing unnecessary _text sub-property
+ * @param {any[]} data an object in the form of { property: { _text: value } }
+ * @returns formatted data in for the form of { property: value }
+ */
+export function formatTextProperty(data) {
+    for (const key of Object.keys(data)) {
+        data[key] = data[key]._text;
+        if (!isNaN(Number(data[key]))) {
+            data[key] = Number(data[key]);
+        }
+    }
+
+    return data;
+}
