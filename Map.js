31ba8c72ae671dbf142a5d1b23bf3ab2800b1c94
Tikki <35748816+tikkikkit21@users.noreply.github.com>
2024-05-04 10:39:48 -0400

    Add refresh button (#5)
    Add auto-refresh every 30s as well as a manual refresh button with 5s
    cooldown

--- Map.js
+++ Map.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useEffect, useState, useRef } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
 import MapView, { Marker } from 'react-native-maps';
-import { FontAwesome6 } from '@expo/vector-icons';
+import { FontAwesome6, FontAwesome } from '@expo/vector-icons';
 import { getAllBuses } from "./controllers/busController";
 import { getColor } from "./controllers/routeController";
 
@@ -13,21 +13,47 @@ export default function Map() {
         latitudeDelta: 0.051202637986392574,
         longitudeDelta: 0.03720943536600885,
     });
+    const refreshTimer = useRef(null);
+    const [isOnCooldown, setIsOnCooldown] = useState(false);
 
     // load bus locations
-    useEffect(() => {
-        async function loadBuses() {
-            const busData = await getAllBuses();
+    async function loadBuses() {
+        const busData = await getAllBuses();
 
-            // add color property
-            for (bus of busData) {
-                bus.color = await getColor(bus.RouteShortName);
-            }
-
-            setBuses(busData)
+        // add color property
+        for (bus of busData) {
+            bus.color = await getColor(bus.RouteShortName);
         }
+
+        setBuses(busData);
+    }
+
+    // auto-refresh bus locations on live map on a set interval
+    useEffect(() => {
         loadBuses();
+        refreshTimer.current = setInterval(() => {
+            loadBuses();
+        }, 30 * 1000);
+
+        return () => {
+            clearInterval(refreshTimer.current);
+        };
     }, []);
+
+    // handles when refresh button is clicked, 5s cooldown
+    function handleRefreshClick() {
+        if (!isOnCooldown) {
+            clearInterval(refreshTimer.current);
+            refreshTimer.current = setInterval(loadBuses, 10000);
+
+            loadBuses();
+
+            setIsOnCooldown(true);
+            setTimeout(() => {
+                setIsOnCooldown(false);
+            }, 5000);
+        }
+    }
 
     const markers = buses.map((bus, index) => {
         return (
@@ -48,7 +74,7 @@ export default function Map() {
         );
     });
 
-    return (
+    return (<>
         <MapView
             style={styles.map}
             region={mapRegion}
@@ -57,12 +83,25 @@ export default function Map() {
         >
             {markers}
         </MapView>
-    );
+        <View style={styles.refreshButton}>
+            <TouchableOpacity onPress={handleRefreshClick}>
+                <FontAwesome name="refresh" size={24} color="white" />
+            </TouchableOpacity>
+        </View>
+    </>);
 }
 
 const styles = StyleSheet.create({
     map: {
         width: '100%',
         height: '100%',
+    },
+    refreshButton: {
+        position: 'absolute',
+        top: 50,
+        right: 10,
+        backgroundColor: '#861F41',
+        padding: 13,
+        borderRadius: 15
     },
 });
