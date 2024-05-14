3f3d708c75ba443da2ce5f1e7c2b801125f7c4ab
tikkikkit21 <tikkikkit21@gmail.com>
2024-05-14 17:36:22 -0400

    Fetch alerts from backend

--- components/Alerts.js
+++ components/Alerts.js
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Text } from "react-native";
+import { getAlerts } from "../controllers/alertController";
 
 export default function Alerts() {
-    
+    const [alerts, setAlerts] = useState([]);
+
+    // load alerts from API
+    useEffect(() => {
+        async function fetchAlerts() {
+            const alertsData = await getAlerts();
+            setAlerts(alertsData);
+        }
+        fetchAlerts();
+    }, []);
+
+    return (<Text>jon has big arms</Text>);
 }
